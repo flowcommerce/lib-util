b0a19928e617afc99485bfd71b6d992b9a59d997
Gregor Heine <gregor@flow.io>
2019-02-20 13:06:58 +0000

    Remove Temporal from DateHelper APIs, provide functions for Instant, ZonedDateTime, OffsetDateTime and LocalDate

--- src/main/scala/io/flow/util/DateHelper.scala
+++ src/main/scala/io/flow/util/DateHelper.scala
@@ -2,7 +2,7 @@ package io.flow.util
 
 import java.time.format.DateTimeFormatter
 import java.time.temporal.{ChronoField, Temporal}
-import java.time.{ZoneId, ZonedDateTime}
+import java.time.{Instant, LocalDate, OffsetDateTime, ZoneId, ZonedDateTime}
 
 object DateHelper {
 
@@ -14,6 +14,8 @@ object DateHelper {
   val CopyrightStartYear: Int = 2016
 
   val EasternTimezone: ZoneId = ZoneId.of("America/New_York")
+
+  val UTCTimeZone: ZoneId = ZoneId.of("UTC")
 
   /**
     * Turns "1" into "01", leaves "12" as "12"
@@ -56,48 +58,104 @@ object DateHelper {
   private[this] val TimeFormat: DateTimeFormatter = DateTimeFormatter.
     ofPattern("HH:mm:ss z")
 
-  def mmmDdYyyy(dateTime: Temporal):  String = {
-    DateTimeFormatter.ofPattern("MMM d, yyyy").format(dateTime)
+  def mmmDdYyyy(instant: Instant):  String = mmmDdYyyy(instant.atZone(UTCTimeZone))
+
+  def mmmDdYyyy(offsetDateTime: OffsetDateTime):  String = mmmDdYyyy(offsetDateTime: Temporal)
+
+  def mmmDdYyyy(zonedDateTime: ZonedDateTime):  String = mmmDdYyyy(zonedDateTime: Temporal)
+
+  def mmmDdYyyy(localDate: LocalDate):  String = mmmDdYyyy(localDate: Temporal)
+
+  private def mmmDdYyyy(temporal: Temporal):  String = {
+    DateTimeFormatter.ofPattern("MMM d, yyyy").format(temporal)
   }
 
-  def shortDate(dateTime: Temporal):  String = {
-    DateTimeFormatter.ofPattern("d/M/yy").format(dateTime)
+  def shortDate(instant: Instant):  String = shortDate(instant.atZone(UTCTimeZone))
+
+  def shortDate(offsetDateTime: OffsetDateTime):  String = shortDate(offsetDateTime: Temporal)
+
+  def shortDate(zonedDateTime: ZonedDateTime):  String = shortDate(zonedDateTime: Temporal)
+
+  def shortDate(localDate: LocalDate):  String = shortDate(localDate: Temporal)
+
+  private def shortDate(temporal: Temporal):  String = {
+    DateTimeFormatter.ofPattern("d/M/yy").format(temporal)
   }
 
-  def shortDateTime(dateTime: Temporal):  String = {
-    shortDate(dateTime) + " " + TimeFormat.format(dateTime)
+  def shortDateTime(instant: Instant):  String = shortDateTime(instant.atZone(UTCTimeZone))
+
+  def shortDateTime(offsetDateTime: OffsetDateTime):  String = shortDateTime(offsetDateTime.toZonedDateTime)
+
+  def shortDateTime(zonedDateTime: ZonedDateTime):  String = {
+    shortDate(zonedDateTime) + " " + TimeFormat.format(zonedDateTime)
   }
 
-  def longDate(dateTime: Temporal):  String = {
-    DateTimeFormatter.ofPattern("MMMM d, yyyy").format(dateTime)
+  def longDate(instant: Instant):  String = longDate(instant.atZone(UTCTimeZone))
+
+  def longDate(offsetDateTime: OffsetDateTime):  String = longDate(offsetDateTime: Temporal)
+
+  def longDate(zonedDateTime: ZonedDateTime):  String = longDate(zonedDateTime: Temporal)
+
+  def longDate(localDate: LocalDate):  String = longDate(localDate: Temporal)
+
+  private def longDate(temporal: Temporal):  String = {
+    DateTimeFormatter.ofPattern("MMMM d, yyyy").format(temporal)
   }
 
-  def longDateTime(dateTime: Temporal):  String = {
-    longDate(dateTime) + " " + TimeFormat.format(dateTime)
+  def longDateTime(instant: Instant):  String = longDateTime(instant.atZone(UTCTimeZone))
+
+  def longDateTime(offsetDateTime: OffsetDateTime):  String = longDateTime(offsetDateTime.toZonedDateTime)
+
+  def longDateTime(zonedDateTime: ZonedDateTime):  String = {
+    longDate(zonedDateTime) + " " + TimeFormat.format(zonedDateTime)
   }
 
-  def consoleLongDateTime(dateTime: Temporal):  String = {
-    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z").format(dateTime)
+  def consoleLongDateTime(instant: Instant):  String = consoleLongDateTime(instant.atZone(UTCTimeZone))
+
+  def consoleLongDateTime(offsetDateTime: OffsetDateTime):  String = consoleLongDateTime(offsetDateTime.toZonedDateTime)
+
+  def consoleLongDateTime(zonedDateTime: ZonedDateTime):  String = {
+    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z").format(zonedDateTime)
   }
 
-  def filenameDateTime(dateTime: Temporal):  String = {
-    filenameDateTimeFormatter.format(dateTime)
+  def filenameDateTime(instant: Instant):  String = filenameDateTime(instant.atZone(UTCTimeZone))
+
+  def filenameDateTime(offsetDateTime: OffsetDateTime):  String = filenameDateTime(offsetDateTime.toZonedDateTime)
+
+  def filenameDateTime(zonedDateTime: ZonedDateTime):  String = {
+    filenameDateTimeFormatter.format(zonedDateTime)
   }
 
   /**
     * Returns the specified date (defaults to now) as a string like
     * "201509"
     */
-  def yyyymm(dateTime: Temporal):  String = {
-    s"${dateTime.get(ChronoField.YEAR)}${DateHelper.prefixZero(dateTime.get(ChronoField.MONTH_OF_YEAR))}"
+  def yyyymm(instant: Instant):  String = yyyymm(instant.atZone(UTCTimeZone))
+
+  def yyyymm(offsetDateTime: OffsetDateTime):  String = yyyymm(offsetDateTime: Temporal)
+
+  def yyyymm(zonedDateTime: ZonedDateTime):  String = yyyymm(zonedDateTime: Temporal)
+
+  def yyyymm(localDate: LocalDate):  String = yyyymm(localDate: Temporal)
+
+  private def yyyymm(temporal: Temporal):  String = {
+    s"${temporal.get(ChronoField.YEAR_OF_ERA)}${DateHelper.prefixZero(temporal.get(ChronoField.MONTH_OF_YEAR))}"
   }
 
   /**
     * Returns the specified date (defaults to now) as a string like
     * "201509"
     */
-  def yyyymmdd(dateTime: Temporal):  String = {
-    yyyymm(dateTime) + DateHelper.prefixZero(dateTime.get(ChronoField.MONTH_OF_YEAR))
+  def yyyymmdd(instant: Instant):  String = yyyymmdd(instant.atZone(UTCTimeZone))
+
+  def yyyymmdd(offsetDateTime: OffsetDateTime):  String = yyyymmdd(offsetDateTime: Temporal)
+
+  def yyyymmdd(zonedDateTime: ZonedDateTime):  String = yyyymmdd(zonedDateTime: Temporal)
+
+  def yyyymmdd(localDate: LocalDate):  String = yyyymmdd(localDate: Temporal)
+
+  private def yyyymmdd(temporal: Temporal):  String = {
+    yyyymm(temporal) + DateHelper.prefixZero(temporal.get(ChronoField.MONTH_OF_YEAR))
   }
 
 }
--- src/test/scala/io/flow/util/DateHelperSpec.scala
+++ src/test/scala/io/flow/util/DateHelperSpec.scala
@@ -1,71 +1,208 @@
 package io.flow.util
 
-import java.time.{OffsetDateTime, ZoneId, ZoneOffset}
+import java.time.{Instant, LocalDate, OffsetDateTime, ZoneOffset, ZonedDateTime}
 import java.time.format.DateTimeFormatter
 
 import org.scalatest.{MustMatchers, WordSpecLike}
 
 class DateHelperSpec extends WordSpecLike with MustMatchers {
 
-  private[this] val jan1 = OffsetDateTime.from(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse("2016-01-01T08:26:18.794-05:00"))
-    .atZoneSameInstant(DateHelper.EasternTimezone)
+  private[this] val jan1OffsetDT: OffsetDateTime = OffsetDateTime.from(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse("2016-01-01T08:26:18.794-05:00"))
 
-  "yyyymm" in {
-    DateHelper.yyyymm(jan1) must equal("201601")
+  private[this] val jan1Instant: Instant = jan1OffsetDT.toInstant
+
+  private[this] val jan1ZonedDT: ZonedDateTime = jan1OffsetDT.atZoneSameInstant(DateHelper.EasternTimezone)
+
+  private[this] val jan1LocalDate: LocalDate = jan1OffsetDT.toLocalDate
+
+
+  "Instant" should {
+    "yyyymm" in {
+      DateHelper.yyyymm(jan1Instant) must equal("201601")
+    }
+
+    "yyyymmdd" in {
+      DateHelper.yyyymmdd(jan1Instant) must equal("20160101")
+    }
+
+    "mmmDdYyyy" in {
+      DateHelper.mmmDdYyyy(jan1Instant) must equal("Jan 1, 2016")
+    }
+
+    "shortDate" in {
+      DateHelper.shortDate(jan1Instant) must equal("1/1/16")
+    }
+
+    "shortDateTime" in {
+      DateHelper.shortDateTime(jan1Instant) must equal("1/1/16 13:26:18 UTC")
+    }
+
+    "longDate" in {
+      DateHelper.longDate(jan1Instant) must equal("January 1, 2016")
+    }
+
+    "longDateTime" in {
+      DateHelper.longDateTime(jan1Instant) must equal("January 1, 2016 13:26:18 UTC")
+    }
+
+    "consoleLongDateTime" in {
+      DateHelper.consoleLongDateTime(jan1Instant) must equal("2016-01-01 13:26:18 UTC")
+    }
+
+    "currentYear" in {
+      DateHelper.currentYear >= 2016
+      DateHelper.currentYear <= OffsetDateTime.now.getYear + 1
+    }
+
+    "copyrightYear" in {
+      val value = DateHelper.copyrightYears
+      Seq("2016", s"2016 - ${DateHelper.currentYear}").contains(value) mustBe(true)
+    }
+
+    "filenameDateTime" in {
+      DateHelper.filenameDateTime(jan1Instant) mustBe "20160101.132618.794"
+    }
   }
 
-  "yyyymmdd" in {
-    DateHelper.yyyymmdd(jan1) must equal("20160101")
+  "OffsetDateTime" should {
+    "yyyymm" in {
+      DateHelper.yyyymm(jan1OffsetDT) must equal("201601")
+    }
+
+    "yyyymmdd" in {
+      DateHelper.yyyymmdd(jan1OffsetDT) must equal("20160101")
+    }
+
+    "mmmDdYyyy" in {
+      DateHelper.mmmDdYyyy(jan1OffsetDT) must equal("Jan 1, 2016")
+    }
+
+    "shortDate" in {
+      DateHelper.shortDate(jan1OffsetDT) must equal("1/1/16")
+    }
+
+    "shortDateTime" in {
+      DateHelper.shortDateTime(jan1OffsetDT) must equal("1/1/16 08:26:18 -05:00")
+    }
+
+    "longDate" in {
+      DateHelper.longDate(jan1OffsetDT) must equal("January 1, 2016")
+    }
+
+    "longDateTime" in {
+      DateHelper.longDateTime(jan1OffsetDT) must equal("January 1, 2016 08:26:18 -05:00")
+    }
+
+    "consoleLongDateTime" in {
+      DateHelper.consoleLongDateTime(jan1OffsetDT) must equal("2016-01-01 08:26:18 -05:00")
+    }
+
+    "currentYear" in {
+      DateHelper.currentYear >= 2016
+      DateHelper.currentYear <= OffsetDateTime.now.getYear + 1
+    }
+
+    "copyrightYear" in {
+      val value = DateHelper.copyrightYears
+      Seq("2016", s"2016 - ${DateHelper.currentYear}").contains(value) mustBe(true)
+    }
+
+    "filenameDateTime" in {
+      DateHelper.filenameDateTime(jan1OffsetDT) mustBe "20160101.082618.794"
+    }
   }
 
-  "mmmDdYyyy" in {
-    DateHelper.mmmDdYyyy(jan1) must equal("Jan 1, 2016")
+  "ZonedDateTime" should {
+    "yyyymm" in {
+      DateHelper.yyyymm(jan1ZonedDT) must equal("201601")
+    }
+
+    "yyyymmdd" in {
+      DateHelper.yyyymmdd(jan1ZonedDT) must equal("20160101")
+    }
+
+    "mmmDdYyyy" in {
+      DateHelper.mmmDdYyyy(jan1ZonedDT) must equal("Jan 1, 2016")
+    }
+
+    "shortDate" in {
+      DateHelper.shortDate(jan1ZonedDT) must equal("1/1/16")
+    }
+
+    "shortDateTime" in {
+      DateHelper.shortDateTime(jan1ZonedDT) must equal("1/1/16 08:26:18 EST")
+    }
+
+    "longDate" in {
+      DateHelper.longDate(jan1ZonedDT) must equal("January 1, 2016")
+    }
+
+    "longDateTime" in {
+      DateHelper.longDateTime(jan1ZonedDT) must equal("January 1, 2016 08:26:18 EST")
+    }
+
+    "consoleLongDateTime" in {
+      DateHelper.consoleLongDateTime(jan1ZonedDT) must equal("2016-01-01 08:26:18 EST")
+    }
+
+    "currentYear" in {
+      DateHelper.currentYear >= 2016
+      DateHelper.currentYear <= OffsetDateTime.now.getYear + 1
+    }
+
+    "copyrightYear" in {
+      val value = DateHelper.copyrightYears
+      Seq("2016", s"2016 - ${DateHelper.currentYear}").contains(value) mustBe(true)
+    }
+
+    "filenameDateTime" in {
+      DateHelper.filenameDateTime(jan1ZonedDT) mustBe "20160101.082618.794"
+      DateHelper.filenameDateTime(jan1ZonedDT.withZoneSameInstant(ZoneOffset.UTC)) mustBe "20160101.132618.794"
+    }
   }
 
-  "shortDate" in {
-    DateHelper.shortDate(jan1) must equal("1/1/16")
+  "LocalDate" should {
+    "yyyymm" in {
+      DateHelper.yyyymm(jan1LocalDate) must equal("201601")
+    }
+
+    "yyyymmdd" in {
+      DateHelper.yyyymmdd(jan1LocalDate) must equal("20160101")
+    }
+
+    "mmmDdYyyy" in {
+      DateHelper.mmmDdYyyy(jan1LocalDate) must equal("Jan 1, 2016")
+    }
+
+    "shortDate" in {
+      DateHelper.shortDate(jan1LocalDate) must equal("1/1/16")
+    }
+
+    "longDate" in {
+      DateHelper.longDate(jan1LocalDate) must equal("January 1, 2016")
+    }
+
+    "currentYear" in {
+      DateHelper.currentYear >= 2016
+      DateHelper.currentYear <= OffsetDateTime.now.getYear + 1
+    }
+
+    "copyrightYear" in {
+      val value = DateHelper.copyrightYears
+      Seq("2016", s"2016 - ${DateHelper.currentYear}").contains(value) mustBe(true)
+    }
   }
 
-  "shortDateTime" in {
-    DateHelper.shortDateTime(jan1) must equal("1/1/16 08:26:18 EST")
+  "DateHelper" should {
+    "implicit ordering" in {
+      val now = OffsetDateTime.now
+      val nowPlus1 = now.plusMinutes(1)
+      val nowPlus5 = now.plusMinutes(5)
+      val nowPlus10 = now.plusMinutes(10)
+
+      val datetimes = Seq(nowPlus10, nowPlus5, nowPlus1, now)
+
+      datetimes.sorted must equal(datetimes.reverse)
+    }
   }
-
-  "longDate" in {
-    DateHelper.longDate(jan1) must equal("January 1, 2016")
-  }
-
-  "longDateTime" in {
-    DateHelper.longDateTime(jan1) must equal("January 1, 2016 08:26:18 EST")
-  }
-
-  "consoleLongDateTime" in {
-    DateHelper.consoleLongDateTime(jan1) must equal("2016-01-01 08:26:18 EST")
-  }
-
-  "currentYear" in {
-    DateHelper.currentYear >= 2016
-    DateHelper.currentYear <= OffsetDateTime.now.getYear + 1
-  }
-
-  "copyrightYear" in {
-    val value = DateHelper.copyrightYears
-    Seq("2016", s"2016 - ${DateHelper.currentYear}").contains(value) mustBe(true)
-  }
-
-  "filenameDateTime" in {
-    DateHelper.filenameDateTime(jan1.withZoneSameInstant(ZoneId.of("America/New_York"))) mustBe "20160101.082618.794"
-    DateHelper.filenameDateTime(jan1.withZoneSameInstant(ZoneOffset.UTC)) mustBe "20160101.132618.794"
-  }
-
-  "implicit ordering" in {
-    val now = OffsetDateTime.now
-    val nowPlus1 = now.plusMinutes(1)
-    val nowPlus5 = now.plusMinutes(5)
-    val nowPlus10 = now.plusMinutes(10)
-
-    val datetimes = Seq(nowPlus10, nowPlus5, nowPlus1, now)
-
-    datetimes.sorted must equal(datetimes.reverse)
-  }
-
 }
