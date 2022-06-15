c1f95faaded2953e94374af6a171d257f6a9f52e
Ben Iofel <ben@flow.io>
2022-06-15 13:50:25 -0400

    Compress tests

--- src/test/scala/io/flow/util/clients/RegistrySpec.scala
+++ src/test/scala/io/flow/util/clients/RegistrySpec.scala
@@ -6,74 +6,53 @@ import org.scalatest.wordspec.AnyWordSpec
 import scala.concurrent.Future
 
 class RegistrySpec extends AnyWordSpec with Matchers {
-  private val nonpci = new ProductionRegistry()
-  private val k8s = new K8sProductionRegistry {
+  private val nonk8s = new ProductionRegistry {
+    // outside k8s, the domains don't resolve
+    override protected def asyncDnsLookupByName(name: String): Future[Unit] =
+      Future.failed(new Exception("invalid domain"))
+  }
+  private val k8s = new ProductionRegistry {
+    // in k8s, the domains should all resolve
     override protected def asyncDnsLookupByName(name: String): Future[Unit] =
       Future.successful(())
   }
-  private val k8sWithTimeout = new K8sProductionRegistry {
+  private val k8sWithTimeout = new ProductionRegistry {
     override protected def asyncDnsLookupByName(name: String): Future[Unit] =
       Future(Thread.sleep(1000000))
   }
   private val mock = new MockRegistry()
 
+  val services = Seq("payment", "session", "experience", "experience-jobs")
+
   "NonK8sProductionRegistry" should {
-    "payment" in {
-      nonpci.host("payment") shouldBe "https://payment.api.flow.io"
-    }
-    "session" in {
-      nonpci.host("session") shouldBe "https://session.api.flow.io"
-    }
-    "experience-api" in {
-      nonpci.host("experience") shouldBe "https://experience.api.flow.io"
-    }
-    "experience-jobs" in {
-      nonpci.host("experience-jobs") shouldBe "https://experience-jobs.api.flow.io"
+    services.foreach { service =>
+      s"resolve $service" in {
+        nonk8s.host(service) shouldBe s"https://$service.api.flow.io"
+      }
     }
   }
 
   "K8sProductionRegistry" should {
-    "payment" in {
-      k8s.host("payment") shouldBe "http://payment"
-    }
-    "session" in {
-      k8s.host("session") shouldBe "http://session"
-    }
-    "experience-api" in {
-      k8s.host("experience") shouldBe "http://experience"
-    }
-    "experience-jobs" in {
-      k8s.host("experience-jobs") shouldBe "http://experience-jobs"
+    services.foreach { service =>
+      s"resolve $service" in {
+        k8s.host(service) shouldBe s"http://$service"
+      }
     }
   }
 
   "ProductionRegistry with DNS Timeout" should {
-    "payment" in {
-      k8sWithTimeout.host("payment") shouldBe "https://payment.api.flow.io"
-    }
-    "session" in {
-      k8sWithTimeout.host("session") shouldBe "https://session.api.flow.io"
-    }
-    "experience-api" in {
-      k8sWithTimeout.host("experience") shouldBe "https://experience.api.flow.io"
-    }
-    "experience-jobs" in {
-      k8sWithTimeout.host("experience-jobs") shouldBe "https://experience-jobs.api.flow.io"
+    services.foreach { service =>
+      s"resolve $service" in {
+        k8sWithTimeout.host(service) shouldBe s"https://$service.api.flow.io"
+      }
     }
   }
 
   "MockRegistry" should {
-    "payment" in {
-      mock.host("payment") shouldBe "http://payment.localhost"
-    }
-    "session" in {
-      mock.host("session") shouldBe "http://session.localhost"
-    }
-    "experience-api" in {
-      mock.host("experience") shouldBe "http://experience.localhost"
-    }
-    "experience-jobs" in {
-      mock.host("experience-jobs") shouldBe "http://experience-jobs.localhost"
+    services.foreach { service =>
+      s"resolve $service" in {
+        mock.host(service) shouldBe s"http://$service.localhost"
+      }
     }
   }
 }
